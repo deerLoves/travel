2247597c3db43c071e3e3339f0ed2453e94e1986
Baidongf <43285249+Baidongf@users.noreply.github.com>
2018-11-12 18:22:29 +0800

    Update myapp/src/store/index.js

---
+++ myapp/src/store/index.js
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+Vue.use(Vuex);
+
+const state = {
+
+}
+
+const getters = {
+
+}
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+const store = new Vuex.Store({
+    state,
+    getters,
+    mutations,
+    actions
+})
+export default store;
