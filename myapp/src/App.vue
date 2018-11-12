06ddea18ab13a40f902bc94ae4743fb847fe7778
Baidongf <43285249+Baidongf@users.noreply.github.com>
2018-11-12 18:22:07 +0800

    Update myapp/src/App.vue

---
+++ myapp/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <tab-com></tab-com>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import tab from "./components/common/tab";
+import Vuex from 'vuex';
+export default {
+  components: {
+      "tab-com":tab
+  }
+}
+</script>
+
+<style>
+#app {
+  height: 100%;
+}
+.page{
+  /*公共样式:上面的电池栏、下面的切换栏*/
+  padding-top: .4rem;
+  padding-bottom: .96rem;
+}
+</style>
