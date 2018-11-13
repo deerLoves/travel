bb9861813784d4c175293e7d8a76c76e1a0756e5
Baidongf <43285249+Baidongf@users.noreply.github.com>
2018-11-13 15:38:38 +0800

    成型首页

--- myapp/src/components/home/home.vue
+++ myapp/src/components/home/home.vue
@@ -2,40 +2,100 @@
     <div class="page home">
         <div id="search">
             <div class="searchspot">
-                <p><i></i>搜“热门地点”试试</p>
+                <i class="iconfont icon-magnifier"></i><p>搜“热门地点”试试</p>
             </div>
             <div class="searchaddress">
-                <p>北京</p>
+                <p>北京</p><i class="iconfont icon-ico_arrows_unfold"></i>
             </div>
         </div>
         <div id="find">
             <h2>发现行漫</h2>
-            <div class="banner"></div>
+            <div class="banner">
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide" v-for="(item,index) in find"><img :src="find[index].photo"></div>
+                    </div>
+                    <!-- 如果需要分页器 -->
+                    <div class="swiper-pagination"></div>
+                </div>
+            </div>
         </div>
         <div id="hotspot">
             <h2>热门景点</h2>
             <div class="banner">
-                <dl>
-                    <dt><img src="../../../static/bai/hotspot/马尔代夫.png"></dt>
-                    <dd><p><span>这行最多13个字</span>这行最多13个字他，来一场属于你</p></dd>
-                    <dd><p>为你而规划</p></dd>
+                <dl v-for="(item,index) in hotspot">
+                    <dt><img :src="hotspot[index].photo"></dt>
+                    <dd><p><span>【{{hotspot[index].name}}】</span>{{hotspot[index].ftitle}}</p></dd>
+                    <dd><p>{{hotspot[index].stitle}}</p></dd>
                 </dl>
             </div>
+
             <div class="more"><p>查看更多热门景点</p></div>
         </div>
         <div id="hottrip">
-
+            <h2>热门行程</h2>
+            <div class="banner">
+                <dl v-for="(item,index) in obj">
+                    <dt><img :src="obj[index].photo"></dt>
+                    <dd><p><span>【{{obj[index].name}}】</span>{{obj[index].ftitle}}</p></dd>
+                    <dd><p>{{obj[index].stitle}}</p></dd>
+                </dl>
+            </div>
         </div>
         <div id="hotstory">
-
+            <h2>热门故事</h2>
+            <h3>一些游客自己的亲身体验</h3>
+            <div class="banner">
+                <dl v-for="(item,index) in hotstory">
+                    <dt><img :src="hotstory[index].photo"></dt>
+                    <dd>{{hotstory[index].ftitle}}</dd>
+                    <dd>{{hotstory[index].stitle}}</dd>
+                </dl>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import axios from "axios";
+import Swiper from "swiper";
+import "../../../node_modules/swiper/dist/css/swiper.min.css"
 export default {
-
+    created(){
+        axios({
+            method:"get",
+            url:"../../../static/bai/josn.json"
+        }).then((data)=>{
+            this.obj=data.data.data,
+            this.hotspot=data.data.hotspot,
+            this.hotstory=data.data.hotstory,
+            this.find=data.data.find
+        })
+    },
+    data(){
+        return{
+            obj:[],
+            hotspot:[],
+            hotstory:[],
+            find:[]
+        }
+    },
+    updated(){
+        this.$nextTick(()=>{
+             new Swiper ('.swiper-container', {
+            loop: true, // 循环模式选项
+            autoplay:{
+                disableOnInteraction:false,
+            },
+            // 如果需要分页器
+            pagination: {
+            el: '.swiper-pagination',
+            },
+        }) 
+        })
+    }
 }
+
 </script>
 
 <style scoped>
@@ -59,15 +119,24 @@ export default {
     color: #030303;
     letter-spacing: -0.003rem;
     line-height: 0.68rem;
+    display: flex;
 }
+#search>.searchspot>i{
+    font-size: 28px;
+    margin-right: 0.16rem;
+}
+
 #search>.searchaddress{
     font-size: 28px;
     line-height: 0.68rem;
     padding-left: 0.14rem;
-    float: left;
+    display: flex;
 }
-
-#find>h2,#hotspot>h2{
+#search>.searchaddress>i{
+    font-size: 0.28rem;
+    margin-left: 0.02rem;
+}
+#find>h2,#hotspot>h2,#hottrip>h2,#hotstory>h2{
     font-family: PingFangSC-Regular;
     font-size: 52px;
     color: #030303;
@@ -77,21 +146,30 @@ export default {
 }
 #find>.banner{
     height: 4.72rem;
-    background: orchid;
     padding:0.54rem 0 0.78rem;
+    display: flex;
+    align-items: center;
 }
-#hotspot>.banner{
+#find>.banner>.swiper-container>.swiper-wrapper>.swiper-slide>img{
+    border-radius: 0.2rem;
+    width: 100%;
+    height: 3.4rem;
+    
+}
+#hotspot>.banner,#hottrip>.banner{
+    display: flex;
+    overflow:scroll;
     padding:0.48rem 0 0.36rem;
 }
-#hotspot>.banner>dl{
+#hotspot>.banner>dl,#hottrip>.banner>dl{
     padding:0 0 0.1rem 0.32rem;
 }
-#hotspot>.banner>dl>dt>img{
+#hotspot>.banner>dl>dt>img,#hottrip>.banner>dl>dt>img{
     width: 4.02rem;
     height: 2.04rem;
-    border-radius: 10px 10px 0 0;
+    border-radius: 0.2rem 0.2rem 0 0;
 }
-#hotspot>.banner>dl>dd:nth-of-type(1)>p{
+#hotspot>.banner>dl>dd:nth-of-type(1)>p,#hottrip>.banner>dl>dd:nth-of-type(1)>p{
     width: 3.78rem;
     font-family: PingFangSC-Medium;
     font-size: 28px;
@@ -99,10 +177,17 @@ export default {
     line-height: 0.4rem;
     letter-spacing: -0.0036rem;
 }
-#hotspot>.banner>dl>dd:nth-of-type(1)>p>span{
+#hotspot>.banner>dl>dd:nth-of-type(1),#hottrip>.banner>dl>dd:nth-of-type(1){
+    height: 0.8rem;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    overflow: hidden;
+}
+#hotspot>.banner>dl>dd:nth-of-type(1)>p>span,#hottrip>.banner>dl>dd:nth-of-type(1)>p>span{
     color: #C90000;
 }
-#hotspot>.banner>dl>dd:nth-of-type(2)>p{
+#hotspot>.banner>dl>dd:nth-of-type(2)>p,#hottrip>.banner>dl>dd:nth-of-type(2)>p{
     font-family: PingFangSC-Regular;
     font-size: 36px;
     color: #D94A4A;
@@ -120,8 +205,57 @@ export default {
     font-size: 24px;
     color: #4D9EE6;
     letter-spacing: -0.003rem;
-    border: 1px solid #4D9EE6;
+    border: 0.02rem solid #4D9EE6;
     line-height: 0.88rem;
     text-align: center;
+}
+#hottrip{
+    padding: 0.74rem 0 0.64rem;
+}
+#hotstory>h3{
+    opacity: 0.5;
+    font-family: PingFangSC-Regular;
+    font-size: 32px;
+    color: #030303;
+    letter-spacing: -0.003rem;
+    line-height: 0.38rem;
+    padding: 0.12rem 0 0.24rem 0.32rem;
+}
+#hotstory>.banner{
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    padding:0.22rem 0.32rem 0;
+}
+#hotstory>.banner>dl{
+    width: 3rem;
+    height:3.4rem;
+    background: #FFFFFF;
+    box-shadow: 0 0.02rem 0.04rem 0 rgba(0,0,0,0.50);
+    border-radius: 0.2rem;
+    margin-bottom: 0.28rem;
+}
+#hotstory>.banner>dl>dt>img{
+    width: 3rem;
+    height: 1.92rem;
+    border-radius: 0.2rem 0.2rem 0 0; 
+}
+#hotstory>.banner>dl>dd:nth-of-type(1){
+    width: 2.5rem;
+    line-height: 0.4rem;
+    font-family: PingFangSC-Medium;
+    font-size: 28px;
+    color: #D94A4A;
+    letter-spacing: -0.0036rem;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+}
+#hotstory>.banner>dl>dd:nth-of-type(2){
+    width: 2.8rem;
+    font-family: PingFangSC-Regular;
+    font-size: 24px;
+    color: #030303;
+    letter-spacing: -0.003rem;
 }
 </style>
