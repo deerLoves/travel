a19621302d72c913e97b606ca72a88108ea4bca8
CZ-chenzhuang <993307840@qq.com>
2018-11-13 12:08:43 +0800

    行程首页初稿

--- myapp/src/components/route/header.vue
+++ myapp/src/components/route/header.vue
@@ -2,7 +2,7 @@
     <div class="header">
         <h2>行程</h2>
         <a href="#">
-            <img src="" alt="">
+            <img src="../../../static/img-chen/route-head_03.png" alt="">
         </a>
     </div>
 </template>
@@ -18,7 +18,14 @@ export default {
         /* display: flex; */
     }
     .header>h2{
-        width:100%;
-        height: 1.68rem;
+        font-family: PingFangSC-Regular;
+        font-size: 48px;
+        color: #000;
+        margin-bottom: .52rem;
     }
+  .header a img{
+    width: 6.86rem;
+    height: 5.98rem;
+    margin-bottom: 40px;
+  }
 </style>
---
+++ myapp/src/components/route/routePlay.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="play-btn">
+      <a href="#" :class="btnindex == 1 ? 'active' : ''"  @click="handleSendIndex(1)">我的行程</a>
+      <a href="#" :class="btnindex == 2 ? 'active' : ''"  @click="handleSendIndex(2)">游客行程</a>
+    </div>
+
+    <component :is="comName" @handleCom="handleChangeCom"></component>
+    <!--<routePath-com></routePath-com>-->
+    <!--<playBtn-com></playBtn-com>-->
+  </div>
+</template>
+
+
+<script>
+    import playBtn from './routeAddBtn.vue'
+    import routePath from './routePath.vue'
+    export default{
+        data(){
+            return{
+                btnindex:1,
+                comName : 'playBtn-com'
+            }
+        },
+      components:{
+        'playBtn-com' :   playBtn,
+        'routePath-com' : routePath
+      },
+      methods:{
+        handleSendIndex(index){
+            this.btnindex = index
+        },
+        handleChangeCom(){
+            this.comName = 'routePath-com'
+        }
+      }
+    }
+</script>
+
+
+<style scoped>
+.play-btn{
+  text-align: center;
+}
+.play-btn a{
+  font-family: PingFangSC-Regular;
+  font-size: 40px;
+  margin:40px 30px 0 0;
+}
+  .active{
+    color: #4E94D0;
+  }
+</style>
