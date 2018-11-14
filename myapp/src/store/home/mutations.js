2a48847ed47e3174713c67d34b6ab1aab163dede
Baidongf <43285249+Baidongf@users.noreply.github.com>
2018-11-14 20:42:19 +0800

    Update myapp/src/store/home/mutations.js

--- myapp/src/store/home/mutations.js
+++ myapp/src/store/home/mutations.js
@@ -1,3 +1,10 @@
 export default {
- 
+    handleindex(state,params){
+        state.IndexObj.code = params.code;
+        state.IndexObj.data = params.data;
+        state.IndexObj.message = params.message;
+        //console.log(1,state.data)
+        // state.headerImg.push('http://ceshi.qfjava.cn/'+params.data.headimg);
+    }
+    
 }
