6b1ea8769bc94c96b8d7d1f6abe4f0f2c9126a35
liu <minifull_y@163.com>
2018-11-14 21:18:09 +0800

    my

---
+++ myapp/src/components/my/inforMark/sex.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="sex" v-show="show">
+        <p>男</p>
+        <p>女</p>
+
+    </div>
+    
+</template>
+<script>
+export default {
+     created(){		
+			this.observer.$on("handleSex",(val)=>{
+				this.show=val;
+			})
+        },
+     data(){
+			return {
+				show:false,				
+			}			
+		},
+   
+       
+}
+</script>
+<style scoped>
+.sex{
+    width:6.86rem;
+    height:3.2rem;
+    background:#FFFFFF ;
+	display: flex;
+	flex-direction: column;
+    justify-content:space-around;
+	position: fixed;
+	left: 0;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	margin: auto;
+	border-radius: .1rem;
+	z-index: 1;
+
+}
+p{
+    text-align:center;
+    font-size:.48rem;
+    font-family: fantasyPingFangSC-Regula;
+    color:#7C7C7C  ;
+}
+
+
+
+
+
+</style>
+
