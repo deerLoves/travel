3e04d4b2cbabd372b9047c8db1d2430a80c2dcd4
Baidongf <43285249+Baidongf@users.noreply.github.com>
2018-11-15 17:04:18 +0800

    Update myapp/src/components/home/attractions.vue

--- myapp/src/components/home/attractions.vue
+++ myapp/src/components/home/attractions.vue
@@ -1,54 +1,51 @@
 <template>
     <div class="attractions">
           <div class="pic">
-              <img src="../../../static/img/c.png">
+              <img :src="SpotAttarr.photo">
                <i class="iconfont icon-guanbi btn" @click="handleClose()"></i>
           </div>
           <div class="conent">
-              <div class="city">格丹斯克</div>
+              <div class="city">{{SpotAttarr.address}}{{SpotAttarr.name}}</div>
               <div class="con-c">
-                         这行最多20个字 这行最多20个字这行最多
-                        TO 11,000 TONS 波兰商港。位于该国北部
-                        海里，距哥本哈根270海里，距基尔343海
-                        池码头，又分北部和南部。北部有西岸的（
-                        港池沿岸码头线总长1190米，允许最大吃水
-                        于杂货装卸；东岸的威斯特尔普拉特港池沿
-                        1442米，最大吃水10.3米，用于木材。南部
-                        年纪念码头，总长1076米，最大吃水10米，
-                        约8500多米。海港即北港，位于维斯杜拉河
-                        东沿海，为原油、煤炭港区，建于60－70年
-                        10万吨级油轮；煤炭突堤在其南，为直臂式，       
+                {{SpotAttarr.detail}}
               </div>
           </div>
           <div class="bottom">
               <h4>游客评论</h4>
               <div class="tour">
                   <img src="../../../static/img/a.png">
-                 <div class="msg">
-                     <p>小姐姐</p>
-                     <p>2018-11-7</p>
+                  <div class="msg">
+                     <p>{{SpotAttarr.scenicEvas[0].createtime}}</p>
+                     <p>{{SpotAttarr.scenicEvas[0].nickname}}</p>
                  </div>
               </div>
               <div class="discuss">
-                          这行最多20个字 这行最多20个字这行最多
-                            TO 11,000 TONS 波兰商港。位于该国北部
-                            海里，距哥本哈根270海里，距基尔343海
-                            池码头，又分北部和南部。北部有西岸的（
-                            港池沿岸码头线总长1190米，允许最大吃水
-                            于杂货装卸；东岸的威斯特尔普拉特港池沿
-                       
-                     </div>
+                {{SpotAttarr.scenicEvas[0].eva}}
+              </div>
           </div>
     </div>
 </template>
 <script>
+import Vuex from 'vuex'
 export default {
+  computed:{
+      ...Vuex.mapState({
+         SpotAttarr:state=>state.home.SpotAttObj.data,
+        //  SpotUser:state=>state.home.SpotAttObj.scenicEvas
+      })
+    },
+  created(){
+    this.handlespotAtt(this.$route.query);
+    // console.log(this.$route.query);
+  },
   methods:{
-     handleClose(){
-      this.$router.push("/place")
-     }
+    ...Vuex.mapActions({
+      handlespotAtt:"home/handlespotAtt"
+    }),
+    handleClose(){
+      this.$router.back()
+     },
   }
-  
 }
 </script>
 <style scoped>
@@ -96,7 +93,7 @@ export default {
   height: 5.76rem;
   padding: 0 0.32rem;
   font-family: PingFangSC-Regular;
-  font-size: 0.36rem;
+  font-size:0.36rem;
   color: #010101;
   letter-spacing: 0;
   line-height: 0.48rem;
