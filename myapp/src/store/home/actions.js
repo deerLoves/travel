7a2dc8e550f1d016b5103601d82be3a76a11382e
Baidongf <43285249+Baidongf@users.noreply.github.com>
2018-11-15 17:04:29 +0800

    Update myapp/src/store/home/actions.js

--- myapp/src/store/home/actions.js
+++ myapp/src/store/home/actions.js
@@ -19,5 +19,34 @@ export default {
             //data.data.photo = 'http://39.106.6.205/'+ data.data.photo;
             commit("handleindex",data.data);
         })
+    },
+    handlespot({commit}){
+        /*
+            查询用户是否登陆:
+            /trip/judgeflag
+        */
+        axios({
+            method:'get',
+            url:"/travel/scenic/gethistory"
+        }).then((data)=>{
+            commit("handlespot",data.data);
+            console.log(data.data)
+        })
+    },
+    handlespotAtt({commit},val){
+        /*
+            查询用户是否登陆:
+            /trip/judgeflag
+        */
+        axios({
+            method:'get',
+            url:"/travel/scenic/getscenicbyid?id=" + val.id
+        }).then((data)=>{
+            var dataArray = data.data.data;
+            dataArray.photo = 'http://ceshi.qfjava.cn/' + dataArray.photo;
+            console.log(dataArray)
+            commit("handlespotAtt",data.data);
+            // console.log(data.data)
+        })
     }
 }
