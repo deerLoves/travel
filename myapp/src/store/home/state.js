849557e5cee230f51bddc003a1191513d14c5062
Baidongf <43285249+Baidongf@users.noreply.github.com>
2018-11-15 17:04:34 +0800

    Update myapp/src/store/home/state.js

--- myapp/src/store/home/state.js
+++ myapp/src/store/home/state.js
@@ -3,5 +3,15 @@ export default {
         code:0,
         data:[],
         message:""
+    },
+    SpotObj:{
+        code:0,
+        data:[],
+        message:""
+    },
+    SpotAttObj:{
+        code:0,
+        data:[],
+        message:""
     }
 }
